6fd72a27b9e743b92bb093c3e244ca5350577acf
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-10-21 11:54:54 -0400

    Chapter 1 - My First C# Code

---
+++ 1 - my-first-code/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!\n");
+
+Console.WriteLine("First method using WriteLine:");
+Console.WriteLine("This is the first line");
+Console.WriteLine("This is the seconde line\n");
+
+Console.WriteLine("Second method using only Write and WriteLine:");
+Console.WriteLine("This is my third line");
+Console.Write("this is my fourth line");
+
+Console.WriteLine("\n\nThird method using Write and control charact/escape sequence:");
+Console.Write("This is my fifth line \nand this is my sixth line!\n\n");
+
+Console.WriteLine("Chapter 1 - The End!");
