6dd761ceb03f987ab5595ec7b149cc7b3f341eb8
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-10-23 13:21:19 -0400

    chapter 3 - arrays - complete

--- 2 - simple-console-application/modules/3-arrays/Unit7.cs
+++ 2 - simple-console-application/modules/3-arrays/Unit7.cs
@@ -5,12 +5,13 @@ class Unit7
   public static void Execute(){
     Console.WriteLine("Unit7 - Foreach with Array");
 
-    string[] frauds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+    string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 
-    foreach(string fraud in frauds){
-      if (fraud.StartsWith("B"))
+    foreach(string orderID in orderIDs)
+    {
+      if (orderID.StartsWith("B"))
       {
-        Console.WriteLine(fraud);
+        Console.WriteLine(orderID);
       }
     }
   }
